8008fdcdb0ced30ae89c8551c718723d47a1ad7a
NoobCooderr <106863573+NoobCooderr@users.noreply.github.com>
2022-06-11 23:55:34 +0530

    master

--- 1DiminishedRadix.cpp
+++ 1DiminishedRadix.cpp
@@ -8,7 +8,7 @@ int main()
     int range, r, n, size;
     cout << "enter the value of R and n " << endl;
     cin >> r >> n;
-    range = ((pow(r, n)) / 2) - 1; //-((r^n)/2 to + ((r^n)/2))-1
+    range = ((pow(r, n)) / 2) - 1; //-((r^n)/2)-1 to + ((r^n)/2))-1
     size = (pow(r, n)) - 1;        // (r^n)-1
     cout << "Range = ( " << -range << " to + " << range << " )" << endl;
     cout << "Size = " << size;
